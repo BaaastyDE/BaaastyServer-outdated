adcb932c3e61809b579294757e9e90d4df054aeb
Baaasty <46427883+Baaasty@users.noreply.github.com>
2023-01-19 18:52:59 +0100

    Delete MetaInfo.java

--- src/main/java/de/baaasty/baaastyserver/database/dao/user/MetaInfo.java
+++
@@ -1,6 +0,0 @@
-package de.baaasty.baaastyserver.database.dao.user;
-
-import java.sql.Timestamp;
-
-public record MetaInfo(String language, Long onlineTime, Timestamp lastSeen, Timestamp firstJoin) {
-}
